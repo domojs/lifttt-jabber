57c0a9db18215feeac90d3df4a42e6d95e232543
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2016-07-24 15:54:28 +0200

    handled exit event properly

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var Client=require('jabber').Client;
 
+var clients=[];
+
+
+process.on('exit', function(){
+    for(var client of clients)
+        client.end();
+});
+
+function buildClient(port, host, username, password, callback){
+    var client=new Client(port, host, username, password);
+    callback(client);
+    client.on('error', function(error){
+        console.log(error);
+        client.end();
+        clients.removeAt(clients.indexOf(client));
+        buildClient(port, host, username, password, callback);
+    });
+    clients.push(client)
+}
+
 module.exports={
     name:"jabber", 
     "triggers":
@@ -14,16 +34,12 @@ module.exports={
                 {name:"password", displayName:"Mot de passe"}
             ],
             when:function(fields,callback){
-                var client=new Client(fields.port, fields.host, fields.username, fields.password);
-                client.on('motionDetected', function(base64Picture){ 
-                    console.log(base64Picture);
-                    callback({encoding:'base64',date:new Date(), picture:base64Picture, htmlPicture:'data:image/png;base64,'+base64Picture});
-                });
-                client.on('error', function(error){
-                    console.log(error);
-                })
-                process.on('exit', function(){
-                    client.end();
+                
+                buildClient(fields.port, fields.host, fields.username, fields.password, function(client){
+                    client.on('motionDetected', function(base64Picture){ 
+                        console.log(base64Picture);
+                        callback({encoding:'base64',date:new Date(), picture:base64Picture, htmlPicture:'data:image/png;base64,'+base64Picture});
+                    });
                 });
             }
         }
